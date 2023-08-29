e1aa38c1d2eb215c367b6564a4a37ebcc325d95c
Aztenz <aztenz@outlook.com>
2023-08-29 18:33:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-import { createServer } from 'http';
-import { parse } from 'url';
+const http = require('http');
+const url = require('url');
 
 let todoItems = [];
 let nextItemId = 1;
 
-const server = createServer((req, res) => {
-  const { pathname, query } = parse(req.url, true);
+const server = http.createServer((req, res) => {
+  const { pathname, query } = url.parse(req.url, true);
 
   if (req.method === 'GET' && pathname === '/todos') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
@@ -18,11 +18,16 @@ const server = createServer((req, res) => {
     });
 
     req.on('end', () => {
-      const newItem = JSON.parse(body);
-      newItem.id = nextItemId++;
-      todoItems.push(newItem);
-      res.writeHead(201, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(newItem));
+      try {
+        const newItem = JSON.parse(body);
+        newItem.id = nextItemId++;
+        todoItems.push(newItem);
+        res.writeHead(201, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(newItem));
+      } catch (error) {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end('Invalid JSON data');
+      }
     });
   } else if (req.method === 'PUT' && pathname.startsWith('/todos/')) {
     const itemId = parseInt(pathname.substring('/todos/'.length), 10);
@@ -34,17 +39,22 @@ const server = createServer((req, res) => {
     });
 
     req.on('end', () => {
-      const updatedItem = JSON.parse(body);
+      try {
+        const updatedItem = JSON.parse(body);
 
-      const itemIndex = todoItems.findIndex(item => item.id === itemId);
+        const itemIndex = todoItems.findIndex(item => item.id === itemId);
 
-      if (itemIndex !== -1) {
-        todoItems[itemIndex] = { ...todoItems[itemIndex], ...updatedItem };
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(todoItems[itemIndex]));
-      } else {
-        res.writeHead(404);
-        res.end('Todo item not found.');
+        if (itemIndex !== -1) {
+          todoItems[itemIndex] = { ...todoItems[itemIndex], ...updatedItem };
+          res.writeHead(200, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify(todoItems[itemIndex]));
+        } else {
+          res.writeHead(404);
+          res.end('Todo item not found.');
+        }
+      } catch (error) {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end('Invalid JSON data');
       }
     });
   } else if (req.method === 'DELETE' && pathname.startsWith('/todos/')) {
